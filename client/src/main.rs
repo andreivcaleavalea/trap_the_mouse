a135250eccef07f9c87379ab21ce07f0f5efa9b0
andreivcaleavalea <andrei.v.caleavalea@gmail.com>
2024-12-14 20:16:24 +0200

    Basic client-server communication and rooms management

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+fn choose_option() -> i32 {
+    use std::io::{self, Write};
+    use rand::Rng;
+
+    println!("Doresti sa fii repartizat intr-o camera random? : 1");
+    println!("Doresti sa creezi o camera? : 2");
+    println!("Doresti sa intri intr-o camera deja repartizata? : 3");
+
+    let mut input = String::new();
+    io::stdout().flush().expect("Eroare la flush");
+    io::stdin().read_line(&mut input).expect("Eroare la citirea de la tastatură");
+
+    let input = input.trim();
+    let mut res = 0;
+
+    match input.parse::<i32>() {
+        Ok(num) => {
+            if num == 1 {
+                res = -1;
+            } else if num == 2 {
+                let mut rng = rand::rng();
+                let random_number = rng.random_range(1..=100);
+                res = random_number;
+            } else if num == 3 {
+                println!("Introduceti codul camerei:");
+                let mut input = String::new();
+                io::stdout().flush().expect("Eroare la flush");
+                io::stdin().read_line(&mut input).expect("Eroare la citirea de la tastatură");
+                let input = input.trim();
+
+                match input.parse::<i32>() {
+                    Ok(num) => res = num,
+                    Err(e) => println!("Eroare la parsare: {}", e),
+                }
+            }
+        }
+        Err(e) => println!("Eroare la parsare: {}", e),
+    }
+    res
+}
+fn main() -> std::io::Result<()> {
+    let server_address = "127.0.0.1:8080";
+    let mut stream = TcpStream::connect(server_address)?;
+
+    println!("Conectat la serverul {}", server_address);
+
+    let res = choose_option();
+
+    println!("Codul camerei este {res} ");
+    stream.write_all(res.to_string().as_bytes()).expect("Eroare la scriere");
+    stream.flush().expect("Eroare la flush");
+
+    let mut buffer = vec![0; 1024];
+
+    loop {
+
+        let n = stream.read(&mut buffer)?;
+        if n == 0 {
+            println!("Serverul a închis conexiunea.");
+            break;
+        }
+        let response = String::from_utf8_lossy(&buffer[..n]);
+        println!("Mesaj primit de la server: {}", response);
+    }
+
+    thread::sleep(Duration::from_secs(1000));
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,200 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{Arc, RwLock};
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+struct Room {
+    slot1: Option<TcpStream>,
+    slot2: Option<TcpStream>,
+    is_full: bool,
+    code: i32
+}
+
+impl Room {
+    fn new(code: i32) -> Self {
+        Room {
+            slot1: None,
+            slot2: None,
+            is_full: false,
+            code
+        }
+    }
+
+    fn add_to_room(&mut self, stream: &TcpStream) -> bool {
+        if self.slot1.is_none() {
+            self.slot1 = Some(stream.try_clone().unwrap());
+            if self.slot2.is_some() {
+                self.is_full = true;
+            }
+            return true;
+        }
+        if self.slot2.is_none() {
+            self.slot2 = Some(stream.try_clone().unwrap());
+            if self.slot1.is_some() {
+                self.is_full = true;
+            }
+            return true;
+        }
+        false
+    }
+
+    // fn remove_from_room(&mut self, stream: &TcpStream) -> bool {
+    //     if let Some(ref s1) = self.slot1 {
+    //         if stream.peer_addr().unwrap() == s1.peer_addr().unwrap() {
+    //             self.slot1 = None;
+    //             self.is_full = false;
+    //             return true;
+    //         }
+    //     }
+    //     if let Some(ref s2) = self.slot2 {
+    //         if stream.peer_addr().unwrap() == s2.peer_addr().unwrap() {
+    //             self.is_full = false;
+    //             return true;
+    //         }
+    //     }
+    //     false
+    // }
+}
+
+struct RoomsManager {
+    rooms: Vec<Room>,
+}
+
+impl RoomsManager {
+    fn new() -> Self {
+        RoomsManager {
+            rooms: Vec::new(),
+        }
+    }
+
+    fn add_to_room(&mut self, stream: TcpStream) -> i32 {
+        for room in self.rooms.iter_mut() {
+            if room.slot1.is_none() || room.slot2.is_none() {
+                room.add_to_room(&stream);
+                return room.code;
+            }
+        }
+
+        self.rooms.push(Room::new(self.rooms.len() as i32));
+        let room_id = self.rooms.len() - 1;
+        self.rooms[room_id].add_to_room(&stream);
+        room_id as i32
+    }
+    fn add_to_spec_room(&mut self, stream: TcpStream, code: i32) ->i32 {
+        for room in self.rooms.iter_mut() {
+            if room.code == code && ( room.slot1.is_none() || room.slot2.is_none() ) {
+                room.add_to_room(&stream);
+                return room.code;
+            }
+        }
+        -1
+    }
+    fn add_to_new_room(&mut self, stream: &TcpStream, code: i32) -> i32 {
+        self.rooms.push(Room::new(code));
+        let room_id = self.rooms.len() - 1;
+        self.rooms[room_id].add_to_room(stream);
+        code
+    }
+    // fn remove_from_room(&mut self, stream: &TcpStream) {
+    //     for room in &mut self.rooms {
+    //         room.remove_from_room(stream);
+    //     }
+    // }
+
+    fn show_rooms(&self) {
+        println!("Camerele curente:");
+        for room in &self.rooms {
+            println!("{:?} {:?}", room.slot1, room.slot2);
+        }
+    }
+    fn check_room(&mut self, room_id: i32) -> bool {
+        for room in &self.rooms {
+            if room.code == room_id && room.is_full {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn handle_client_1(mut stream: TcpStream, rooms_manager: Arc<RwLock<RoomsManager>>) {
+    // Citim de la client optiunea camerei(una anume/random)
+    let mut buffer: [u8; 100] = [0; 100];
+    let mut room: i32 = -1;
+    let mut rm_id: i32 = -1;
+    match stream.read(&mut buffer) {
+        Ok(n) if n > 0 => {
+            let message = String::from_utf8_lossy(&buffer[..n]);
+            let message = message.trim();
+
+            match message.parse::<i32>() {
+                Ok(num) => {
+                    println!("Cod camera de la client: {}", num);
+                    room = num;
+                }
+                Err(e) => println!("Eroare la parsare: {}", e),
+            }
+        }
+        Ok(0) => {
+            println!("Clientul {} s-a deconectat inainte sa fie repartizat intr-o camera.", stream.peer_addr().unwrap());
+        }
+        Err(e) => {
+            eprintln!("Eroare la citirea datelor de la client: {}", e);
+        }
+        _ => {}
+    }
+    match room {
+       -1 => {
+           let mut rooms_manager = rooms_manager.write().unwrap();
+           rm_id = rooms_manager.add_to_room(stream.try_clone().unwrap());
+           println!("Clientul a fost adăugat în camera {}", rm_id);
+           rooms_manager.show_rooms();
+       }
+        _ => {
+            let mut rooms_manager = rooms_manager.write().unwrap();
+            rm_id = rooms_manager.add_to_spec_room(stream.try_clone().unwrap(), room);
+
+            if rm_id == -1 {
+                rm_id = rooms_manager.add_to_new_room(&stream, room);
+            }
+
+            println!("Clientul a fost adăugat în camera {}", rm_id);
+            rooms_manager.show_rooms();
+        }
+    }
+    loop {
+        {
+            let mut rooms_manager = rooms_manager.write().unwrap();
+            if rooms_manager.check_room(rm_id) {
+                break;
+            }
+        }
+    }
+    let message = format!("Jocul a inceput in camera! {}", rm_id);
+    if let Err(e) = stream.write_all(message.as_bytes()) {
+        eprintln!("Eroare la scriere către client: {}", e);
+    }
+    //Clientul a fost adaugat intr-o camera, iar jocul a inceput
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Serverul rulează pe 127.0.0.1:8080");
+
+    let rooms_manager = Arc::new(RwLock::new(RoomsManager::new()));
+
+    for stream in listener.incoming() {
+        let rooms_manager = Arc::clone(&rooms_manager);
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client_1(stream, rooms_manager.clone());
+                });
+            }
+            Err(e) => {
+                eprintln!("Eroare la acceptarea conexiunii: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
